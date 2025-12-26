a7b101051eec7b303c81d92de37f1c50165957ff
Allan Cassio <allan.cassio1@gmail.com>
2025-12-26 15:21:34 -0300

    added repositories to unit tests

---
+++ tests/repositories/user-repository-in-memory.ts
@@ -0,0 +1,15 @@
+import { User } from '../../src/domain/entities/user-entity'
+import { UserRepository } from '../../src/domain/repositories/user-repository'
+
+export const users: User[] = []
+export class UserRepositoryInMemory implements UserRepository {
+  create(user: User): Promise<User> {
+    users.push(user)
+    return Promise.resolve(user)
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    const user = users.find((user) => user.email === email)
+    return Promise.resolve(user || null)
+  }
+}
