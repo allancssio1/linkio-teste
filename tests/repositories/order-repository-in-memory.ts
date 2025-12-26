9d16fc0ac9e892396424f71e7b8cd7ed59da8cc1
Allan Cassio <allan.cassio1@gmail.com>
2025-12-26 16:49:08 -0300

    create repository in memory of order

---
+++ tests/repositories/order-repository-in-memory.ts
@@ -0,0 +1,29 @@
+import { Order } from '../../src/domain/entities/order-entity'
+import { OrderRepository } from '../../src/domain/repositories/order-repository'
+
+export const orders: Order[] = []
+export class OrderRepositoryInMemory implements OrderRepository {
+  findById(id: string): Promise<Order | null> {
+    const order = orders.find((order) => order.id === id)
+    return Promise.resolve(order || null)
+  }
+  advanceStatus(
+    state: 'CREATED' | 'ANALYSIS' | 'COMPLETED',
+    id: string,
+  ): Promise<void> {
+    const index = orders.findIndex((o) => o.id === id)
+
+    if (index === -1) {
+      throw new Error('User not found')
+    }
+    orders[index].state = state
+    return Promise.resolve()
+  }
+  listOrders(): Promise<Order[]> {
+    return Promise.resolve(orders)
+  }
+  create(order: Order): Promise<Order> {
+    orders.push(order)
+    return Promise.resolve(order)
+  }
+}
