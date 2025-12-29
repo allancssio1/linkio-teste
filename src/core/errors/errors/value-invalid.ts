34643dbe78b0a4be5ec19c8ba6354c789d4da1a4
Allan Cassio <allan.cassio1@gmail.com>
2025-12-29 20:32:18 -0300

    implementando logica de avanço de state

--- src/core/errors/errors/value-invalid.ts
+++ src/core/errors/errors/value-invalid.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from '../error-usecase'
 
 export class ValueInvalidError extends Error implements UseCaseError {
   constructor() {
-    super('Value Service Invalid')
+    super('Value Invalid')
   }
 }
