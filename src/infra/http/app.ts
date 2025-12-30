2d7c440efc0d20adddffd7238221d7eddf6754d8
Allan Cassio <allan.cassio1@gmail.com>
2025-12-30 09:35:59 -0300

    criando server

---
+++ src/infra/http/app.ts
@@ -0,0 +1,83 @@
+import fastify from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fromZodError } from 'zod-validation-error'
+import { ZodError } from '../config/zod-v4'
+// import { userRoutes } from './routes/user-routes'
+import { AppError } from 'src/core/errors/errors/app-error'
+import { env } from '../config/env'
+// import fastifySwaggerUi from '@fastify/swagger-ui'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'User Service',
+      description: 'API for User Service',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+// app.register(fastifySwaggerUi, {
+//   routePrefix: '/docs',
+//   uiConfig: {
+//     docExpansion: 'list',
+//     deepLinking: false,
+//   },
+// })
+
+app.get('/', () => {
+  return { status: 'ok' }
+})
+
+// app.register(userRoutes, {
+//   prefix: '/users',
+// })
+
+app.setErrorHandler((error, _request, reply) => {
+  // if (error.code === 'FST_ERR_VALIDATION') {
+  //   return reply.status(400).send({
+  //     message: 'Validation error.',
+  //     errors: error,
+  //   })
+  // }
+
+  if (error instanceof AppError) {
+    return reply.status(error.statusCode).send({
+      message: 'Validation error.',
+      errors: error,
+    })
+  }
+
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      errors: fromZodError(error),
+    })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
+export { app }
